ea3c982311a912addc6b00b3b050dc296fa66521
Liz <elizabeth_do1995@hotmail.com>
2018-11-30 16:26:43 -0500

    implement and design Registration Controller

--- project/AccountController.swift
+++ project/AccountController.swift
@@ -15,6 +15,16 @@ class AccountController: UIViewController {
     @IBOutlet weak var label_login: UILabel!
     @IBOutlet weak var label_email: UILabel!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
     @IBAction func action_accountSettings(_ sender: UIButton) {
         let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 175, y: self.view.frame.size.height-100, width: 350, height: 80))
         toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
---
+++ project/RegistrationController.swift
@@ -0,0 +1,25 @@
+//
+//  RegistrationController.swift
+//  project
+//
+//  Created by Duong Elizabeth on 11/30/18.
+//  Copyright © 2018 De La Rosa Rivero Francisco. All rights reserved.
+//
+
+import UIKit
+class RegistrationController: UIViewController
+    {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //        textField_firstName.attributedPlaceholder =
+        //            NSAttributedString(string: "First Name", attributes: [NSAttributedStringKey.foregroundColor:])
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
