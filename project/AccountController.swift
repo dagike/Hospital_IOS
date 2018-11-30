f70ef166077499c67938f851c949c370acf1b994
Oleksii Kachan <alexkachan.lutsk@gmail.com>
2018-11-30 12:22:37 -0800

    Implement design of account view

---
+++ project/AccountController.swift
@@ -0,0 +1,14 @@
+//
+//  AccountController.swift
+//  project
+//
+//  Created by Kachan Oleksii on 11/30/18.
+//  Copyright © 2018 De La Rosa Rivero Francisco. All rights reserved.
+//
+
+
+import UIKit
+
+class AccountController: UIViewController {
+    
+}
