f6a63217cf7b0d25b914dcc8de6b02bbde63d020
Liz <elizabeth_do1995@hotmail.com>
2018-11-30 15:38:10 -0500

    implement design and txt for login

--- project/LoginController.swift
+++ project/LoginController.swift
@@ -10,14 +10,49 @@ import UIKit
 
 class LoginContoller: UIViewController {
     
-   
-    @IBOutlet weak var userName: UITextField!
+    let myFileURL = Bundle.main.path(forResource: "myAccount", ofType: "txt")
     
+    @IBOutlet weak var userName: UITextField!
     @IBOutlet weak var password: UITextField!
     
-    @IBAction func testLogin(_ sender: UIButton) {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
         
-        password.text = userName.text!
+        let accountView = UIImageView();
+        let lockView = UIImageView();
+        let account = UIImage(named: "account.png");
+        let lock = UIImage(named: "lock.png");
+        accountView.image = account;
+        lockView.image = lock;
+        userName.leftView = accountView;
+        userName.leftViewMode = UITextFieldViewMode.always
+        userName.leftViewMode = .always
+        password.leftView = lockView;
+        password.leftViewMode = UITextFieldViewMode.always
+        password.leftViewMode = .always
+        accountView.frame = CGRect(x: 5, y: 0, width: userName.frame.height - 5, height: userName.frame.height - 5); view.addSubview(accountView)
+        lockView.frame = CGRect(x: 5, y: 0, width: password.frame.height - 5 , height: password.frame.height - 5); view.addSubview(lockView)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    @IBAction func onLoginClicked(_ sender: UIButton) {
+        var getFileContent = ""
+        do {
+            getFileContent += try String (contentsOfFile: myFileURL!, encoding:String.Encoding.utf8)
+        }catch let error as NSError {
+            print("Failed due to \(error)")
+        }
+        
+//        userName.text = getFileContent
         
     }
+    
+    
 }
