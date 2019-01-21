66198260298708df5cd0a8954df87d82be0920c7
dagike <sinkstillfree@gmail.com>
2019-01-21 12:41:20 -0500

    add Account Settings

---
+++ project/AccountSettingsController.swift
@@ -0,0 +1,24 @@
+//
+//  AccountSettingsControllerViewController.swift
+//  project
+//
+//  Created by De La Rosa Rivero Francisco on 1/21/19.
+//  Copyright © 2019 De La Rosa Rivero Francisco. All rights reserved.
+//
+
+import UIKit
+
+class AccountSettingsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
